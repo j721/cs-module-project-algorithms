23404a58558286aad44a790696b683eb9f1325c5
Julia Vu <juliadvu@gmail.com>
2020-06-16 22:59:06 -0500

    single_number test passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,6 +5,7 @@ Returns: an integer
 
 #UNDERSTAND (U)
 #presented with an array data structure with a list of intergers
+#are there constraints where only positive intergers or numbers less than 0 can be added to the list?
 
 #input
 #list of intergers that contains duplicates, expect for one interger
@@ -15,13 +16,32 @@ Returns: an integer
 
 #PLAN(P)
 #need to loop through the list of intergers to find the one that does not contain the duplicate
+#conditional statements- if there is duplicate interger that return the list
+    #else if the interger is a duplicate
+    #return that interger found in the array index
 
+#EXECUTE (E)
+#how to check if interger is a duplicate or not?
+#create an empty array that will hold the single intergers
+#use append and remove method for array
 
 def single_number(arr):
     # Your code here
 
-    pass
+    #create an array that will hold the single interger
+    single = []
 
+    #loop through every element in the arr
+    for i in arr:
+        if i not in single:
+            single.append(i)
+        #if the i element has already been added to the single array then remove it.  
+        else:
+            single.remove(i)
+    #return the first item from the single array
+    return single[0]
+
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
