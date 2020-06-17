7b7bc754cbb534435df95f54cc42b388b28c4bc1
Julia Vu <juliadvu@gmail.com>
2020-06-17 17:33:37 -0500

    second method for moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -23,6 +23,7 @@ Returns: a List of integers
 #then just return the array back
 
 # Execute (E)
+#need to initialize count to 0
 #use for loop to iterate through array
 # if index at array arr[i] !== 0
   #  return arr
@@ -33,18 +34,36 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
+    #initialize count to zero
+    # count = 0
+    # #loop through array
+    # for i in range(len(arr)):
+    #     #if we encounter a non-zero interger in the array
+    #     if arr[i] !=0:
+    #         #increment by +1 into the counter
+    #         count +=1
+    #         #array order will now be the array at index i, the zero will be at the end [:i], the non-zero intergers will be on the left [i+1] referring to the counter
+    #         arr = [arr[i]] + arr [:i] + arr[i+1:]
+    # return(arr)
+
+
+
+
+
+    #2nd method
+    #Just remove the 0 from the array, and add it back to the end of the array
+    #use Remove method to remove the 0 from array
+    # use append method to add the 0 to the end of the array
+    #still have to loop through array 
+
+
     #initialize count    
-    count = 0
-
-    if arr[0] !=0:
-        return arr
-
-    if arr[i] ==0:
-        for i in arr:
-            if arr[i] !=0:
-                arr[count] = arr[i]
-                count +=1
-
+    count = arr.count(0)
+    #loop through array 
+    for i in range(0, count):
+        arr.remove(0) #remove method to remove 0
+        arr.append(0) #append method adds 0 to the end of the array     #similar to insertion method
+    return arr
 
         
 
