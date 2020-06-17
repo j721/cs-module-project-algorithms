ce52bdc8a3b558ec413d2f18187de4d7008b889a
Julia Vu <juliadvu@gmail.com>
2020-06-16 22:32:10 -0500

    working on single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,6 +2,21 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+#UNDERSTAND (U)
+#presented with an array data structure with a list of intergers
+
+#input
+#list of intergers that contains duplicates, expect for one interger
+
+#output
+#if there is a duplicate number in the list, then just return the list or return None
+#if the interger is not a duplicate, then return that single interger
+
+#PLAN(P)
+#need to loop through the list of intergers to find the one that does not contain the duplicate
+
+
 def single_number(arr):
     # Your code here
 
